215a47a55d772953054eeaff2b65c955a98d7af5
Juan Sebastian <juansebastianvargasposada@gmail.com>
2022-04-13 22:38:36 -0500

    Socket rutas mapa

--- sockets/controller.js
+++ sockets/controller.js
@@ -1,7 +1,19 @@
 const { Socket } = require('socket.io');
 
 const socketController = async( socket = new Socket(), io ) => {
-    
+    io.on('connection', (socket) => {
+        socket.on('find-driver', ({points}) => {
+            console.log(points);
+            const counter = setInterval(() => {
+                const coords = points.shift();
+                if (!coords) {
+                    clearInterval(counter);
+                } else {
+                    socket.emit('position', {coords});
+                }
+            }, 2500)
+        })
+    })
 }
 
 module.exports = {
